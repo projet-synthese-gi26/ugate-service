8f11d21b1b72e2a443b4b65addff01f1ebfc2640
Eric KOGHENE <erickoghene@gmail.com>
2026-01-28 12:33:12 +0100

    fix bug

--- src/main/java/com/yowyob/ugate_service/infrastructure/adapters/inbound/rest/dto/request/ServiceOfferingRequest.java
+++ src/main/java/com/yowyob/ugate_service/infrastructure/adapters/inbound/rest/dto/request/ServiceOfferingRequest.java
@@ -2,6 +2,7 @@ package com.yowyob.ugate_service.infrastructure.adapters.inbound.rest.dto.reques
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
 public record ServiceOfferingRequest(
    
