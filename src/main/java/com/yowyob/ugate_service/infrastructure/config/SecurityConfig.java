cd2c12f93f4f089cf35a426094db0430d1b6b455
Eric KOGHENE <erickoghene@gmail.com>
2026-02-01 14:35:28 +0100

    fix complaince bug

--- src/main/java/com/yowyob/ugate_service/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/yowyob/ugate_service/infrastructure/config/SecurityConfig.java
@@ -44,7 +44,8 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/webjars/**",
                                 "/actuator/**",
-                                "/syndicates"
+                                "/syndicates",
+                                "/compliance/**"
                         ).permitAll()
                         .anyExchange().authenticated()
                 )
